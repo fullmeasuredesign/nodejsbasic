f0e64656ea8c00f8e5fd74986fda5ec09a59e401
David Karasek <djk@fullmeasuredesign.com>
2013-05-21 16:25:03 -0500

    Added ls push to dom

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,20 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(res) {
 	console.log("Request handler 'start' was called.");
+
+	exec("ls -lad", function (error, stdout, stderr) {
+		res.writeHead(200, {"Content-Type": "text/plain"});
+		res.write(stdout);
+		res.end();
+	});
 }
 
-function upload() {
+function upload(res) {
 	console.log("Request handler 'upload' was called.");
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.write("Hello upload");
+	res.end();
 }
 
 exports.start = start;
--- router.js
+++ router.js
@@ -1,5 +1,13 @@
-function route(pathname) {
+function route(handle, pathname, res) {
 	console.log("About to route a request for " + pathname);
+	if (typeof handle[pathname] === 'function') {
+		return handle[pathname](res);
+	} else {
+		console.log("No request handler found for " + pathname);
+		res.writeHead(404, {"Content-Type": "text/plain"});
+		res.write("404 Not found");
+		res.end();
+	}
 }
 
 exports.route = route;
